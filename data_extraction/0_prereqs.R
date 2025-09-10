96e13486c2db4a9910d9e2d86b622e3251b2d27b
Emma Whitfield <60394883+ekw26@users.noreply.github.com>
2025-09-10 11:30:52 +0100

    Update 0_prereqs.R

--- data_extraction/0_prereqs.R
+++ data_extraction/0_prereqs.R
@@ -20,13 +20,14 @@
 # Notes:
 ## ========================================================================== ##
 
-analysis_dt <- "220725"
+analysis_dt <- "010101"
 
 # folder containing individual data files to be referenced
-data_folder <- "S:/IAU/heq/demographics_data"
+data_folder <- "directory of reference files"
 
 # workforce folder contains contents of July 2022 workforce zip file from Downloaded from https://digital.nhs.uk/data-and-information/publications/statistical/general-and-personal-medical-services/31-july-2022
-workforce_folder <- "S:/IAU/heq/demographics_data/GP_workforce/"
+workforce_folder <- "directory of workforce files"
 
 # folder to save outputs
-results_folder <- "S:/IAU/heq/demographics_data/processed"
+results_folder <- "directory for results"
+
